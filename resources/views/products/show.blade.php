a1ed89680967f0d84e38225e99bada3f114b23a2
1oneGod1 <140790868+1oneGod1@users.noreply.github.com>
2025-06-04 08:24:26 +0700

    Add product categories, search, filter, and improved home

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,5 +1,6 @@
 <x-template title="Product Detail">
     <h1>{{ $product['name'] }}</h1>
+    <span class="badge bg-secondary">{{ $product['category'] }}</span>
     <p>{{ $product['description'] }}</p>
     <p><strong>Rp {{ $product['price'] }}</strong></p>
 </x-template>
